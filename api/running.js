bc76f18672bd7ec724dfae1c446f72aac78382e8
cbpgresult-com <sunflixmovies@gmail.com>
2026-07-01 02:30:02 +0530

    Update running.js

--- api/running.js
+++ api/running.js
@@ -1,5 +1,14 @@
 export default async function handler(req, res) {
 
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const train = req.query.train;
 
   if (!train) {
